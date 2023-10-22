616feedd2449d4a143300d9d3d80ba126c685ae7
jbeli oussema <jbelioussema33@gmail.com>
2023-10-23 00:43:51 +0100

    test

--- training.py
+++ training.py
@@ -1,21 +1,47 @@
 import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import scipy
 
+# Load MNIST Data
 mnist = tf.keras.datasets.mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+# Data Normalization
 X_train = tf.keras.utils.normalize(X_train, axis=1)
 X_test = tf.keras.utils.normalize(X_test, axis=1)
 
+# Reshape input data for compatibility with ImageDataGenerator
+X_train = np.expand_dims(X_train, axis=-1)  # Add channel dimension
+X_test = np.expand_dims(X_test, axis=-1)  # Add channel dimension
+
+# Data Augmentation
+datagen = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, zoom_range=0.1)
+
+# Create the Neural Network Model
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=256, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=64, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# Compile the Model with Customized Learning Rate
+optimizer = Adam(learning_rate=0.001)
+model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=3)
+# Early Stopping Callback
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
+# Training the Model with Data Augmentation
+model.fit(datagen.flow(X_train, y_train, batch_size=32), epochs=20, validation_data=(X_test, y_test), callbacks=[early_stopping])
+
+# Evaluating the Model
 val_loss, val_acc = model.evaluate(X_test, y_test)
+print("Validation Loss:", val_loss)
+print("Validation Accuracy:", val_acc)
 
+# Saving the Model
 model.save('training.model')

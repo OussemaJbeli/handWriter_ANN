5b4a2968dbdc10dd502bbb473a397d80703e52d5
jbeli oussema <jbelioussema33@gmail.com>
2023-10-23 13:15:43 +0100

    test

--- main.py
+++ main.py
@@ -29,20 +29,33 @@ def predict_digit(image_path):
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
+#********************** for fraowing with canvas*****************
+@app.route('/predictDrow', methods=['POST'])
+def predictDrow():
     try:
         data = request.get_json()
         if 'image' in data:
             image_data = data['image'].split(',')[1] 
             image_bytes = base64.b64decode(image_data)
-
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'image.png')
             with open(image_path, 'wb') as img_file:
                 img_file.write(image_bytes)
         
         predicted_digit = predict_digit('uploads/test/image.png')
+        if predicted_digit is not None:
+            return jsonify({'predicted_digit': predicted_digit})
+        else:
+            return jsonify({'error': 'Error occurred during prediction.'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+#********************** for img with path *****************
+@app.route('/predictIMG', methods=['POST'])
+def predictIMG():
+    try:
+        image = request.files['image']
+        image_path = 'uploads/test/image.png'
+        image.save(image_path)
+        predicted_digit = predict_digit(image_path)
         if predicted_digit is not None:
             return jsonify({'predicted_digit': predicted_digit})
         else:
